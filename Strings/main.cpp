b28b8e53daa16011a41832836f425b6b1e68d15c
Storm <Storm@STORMSSD>
2024-04-18 00:50:14 +0300

    shrink

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -1,11 +1,14 @@
 ﻿#include<iostream>
 using namespace std;
 
-#define ASCII
+//#define ASCII
 void Print(char str[], const int n);
 int string_length(char str[], const int n);
 void to_lower(char str[], const int n);
 void to_upper(char str[], const int n);
+void shrink(char*& str, const int n);
+int is_int_number(char str[], const int n);
+
 
 
 void main()
@@ -22,12 +25,15 @@ void main()
 	Print(str, n);
 	to_upper(str, n);
 	Print(str, n);
+	shrink(str, n);
+	Print(str, n);
+	cout << is_int_number(str, n);
 		
 
 }
 int string_length(char str[], const int n)
 {
-	for (int i = 0; i < n; i++) if (str[i] == 0) return i;
+	for (int i = 0; i < n; i++) if (str[i] == 0) return i+1;
 	return 0;
 }
 void Print(char str[], const int n)
@@ -54,4 +60,25 @@ void to_upper(char str[], const int n)
 		else if (256+str[i] >= 160 && 256+str[i] <= 175) str[i] = 256+str[i] - 32; //Русские
 		else if (256+str[i]>=224 && 256+str[i]<=239 ) str[i] = 256+str[i] - 80; //Русские
 	}
+}
+
+void shrink(char*& str, const int n)
+{
+	char* bufer = new char[n];
+	bufer[0] = str[0];
+	for (int i = 1,j=0; i < string_length(str, n); i++)
+	{
+		if (bufer[j] == str[i] && bufer[j] == 32) continue;
+		bufer[++j] = str[i];
+	}
+		delete[] str;
+		str = bufer;
+}
+int is_int_number(char str[], const int n)
+{
+	for (int i = 0; i < string_length(str, n) - 1; i++)
+	{
+		if (!(str[i] >= 48 && str[i] <= 57)) return 0;
+	}
+	return 1;
 }
